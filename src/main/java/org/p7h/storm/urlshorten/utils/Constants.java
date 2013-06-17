4d2fb9ded3cb0cade8ba556b3b8756c4a37dd53c
Prashanth Babu <Prashanth.Babu@gmail.com>
2013-06-18 01:45:21 +0530

    Fixed OAuth Spelling mistake.

--- src/main/java/org/p7h/storm/urlshorten/utils/Constants.java
+++ src/main/java/org/p7h/storm/urlshorten/utils/Constants.java
@@ -10,10 +10,10 @@ package org.p7h.storm.urlshorten.utils;
 public class Constants {
 	public static final String TOPOLOGY_NAME = "UnshortenURLs";
 	public static final String CONFIG_PROPERTIES_FILE = "config.properties";
-	public static final String  OATH_ACCESS_TOKEN = "OATH_ACCESS_TOKEN";
-	public static final String OATH_ACCESS_TOKEN_SECRET = "OATH_ACCESS_TOKEN_SECRET";
-	public static final String OATH_CONSUMER_KEY = "OATH_CONSUMER_KEY";
-	public static final String OATH_CONSUMER_SECRET = "OATH_CONSUMER_SECRET";
+	public static final String OAUTH_ACCESS_TOKEN = "OAUTH_ACCESS_TOKEN";
+	public static final String OAUTH_ACCESS_TOKEN_SECRET = "OAUTH_ACCESS_TOKEN_SECRET";
+	public static final String OAUTH_CONSUMER_KEY = "OAUTH_CONSUMER_KEY";
+	public static final String OAUTH_CONSUMER_SECRET = "OAUTH_CONSUMER_SECRET";
 	public static final String TWEET = "tweet";
 	public static final String SHORT_URL = "shortUrl";
 	public static final String EXPANDED_URL = "expUrl";
